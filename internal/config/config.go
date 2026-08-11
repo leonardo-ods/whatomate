f82ef96c32313fd640aba13df9952f1fc920f551
Leonardo Oliveira <159083640+leonardo-ods@users.noreply.github.com>
2026-08-11 10:27:33 -0300

    feat(config): add S3 endpoint configuration

--- internal/config/config.go
+++ internal/config/config.go
@@ -156,12 +156,14 @@ type AIConfig struct {
 }
 
 type StorageConfig struct {
-	Type      string `koanf:"type"` // local, s3
-	LocalPath string `koanf:"local_path"`
-	S3Bucket  string `koanf:"s3_bucket"`
-	S3Region  string `koanf:"s3_region"`
-	S3Key     string `koanf:"s3_key"`
-	S3Secret  string `koanf:"s3_secret"`
+	Type           string `koanf:"type"` // local, s3
+	LocalPath      string `koanf:"local_path"`
+	S3Bucket       string `koanf:"s3_bucket"`
+	S3Region       string `koanf:"s3_region"`
+	S3Key          string `koanf:"s3_key"`
+	S3Secret       string `koanf:"s3_secret"`
+	S3Endpoint     string `koanf:"s3_endpoint"`
+	S3UsePathStyle bool   `koanf:"s3_use_path_style"`
 }
 
 type DefaultAdminConfig struct {
